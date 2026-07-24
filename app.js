8894c2ffc8ca7124762254eb1e416b9999c7b4fd
Vaneesa <vane@gmail.com>
2026-07-24 11:40:03 -0500

    Config variable entorno

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get("/", (_, res) => {
   res.send('Aprendiendo express, ficha 3407181');
 });
 
-app.listen(port, () => {
-  console.log(`Servidor en funcionamiento en el puerto: ${port}`);
+app.listen(port, () => { 
+    console.log( `Servidor en funcionamiento en el puerto: ${port}`); 
 });
