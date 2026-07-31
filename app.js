91c36e7e0fbad633cfd645fa98cf66e6809e6c77
JeanOjeda-Dev <jeanojeda2504@gmail.com>
2026-07-31 10:18:12 -0500

    Aplicacion configurando las variables de entorno

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+//const express = requiere ("express")
 import express from 'express';
+//Leer el archivo .env
+import {configDotenv} from "dotenv"
+configDotenv()
+
 const app = express();
-const port = 3000;
+const port = process.env.PUERTO || 2008
 
 app.get("/", (_, res) => {
-  res.send('Aprendiendo express, ficha 3407181');
+  res.send('Aprendiendo express, ficha 3407181, ADSO en el SENA 31 julio');
 });
+
+//otro endpoint
 
 app.listen(port, () => { 
     console.log( `Servidor en funcionamiento en el puerto: ${port}`); 
