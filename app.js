ac53d84ab8365ba5a7391dfa53c0fb387ec1f8ca
Vane <vane@gmail.com>
2026-08-21 11:35:50 -0500

    inicio segundo proyecto con cambios de funcionamiento y datos en datos.json

--- app.js
+++ app.js
@@ -2,11 +2,26 @@ const express = require('express');
 const app = express();
 require('dotenv/config')
 const port = process.env.port || 3000;
-
-
-app.get("/", (_, res) => {
+//configuracion para la lectura de archivos 
+const sistemaArchivo = require('fs')
+const ruta = require('path')
+const rutaArchivoJson = ruta.join(__dirname, "datos.json");
+//endpoin raiz
+app.get("/", function(req, res){
   res.send('API - REST APRENDICES');
-});
+})
+
+//endpoin para ver los datos del archivo
+app.get("/api/aprendices", (req, res)=> {
+  //datos viene del archivo
+  sistemaArchivo.readFile(rutaArchivoJson, 'utf-8', (error, datos)=>{
+    if(error){
+      return res.json({Error:"no se puede leer los datos"})
+    }
+    const listaAprendices = JSON.parse(datos)
+    res.json(listaAprendices)
+  })
+}) 
 
 app.listen(port, () => {
   console.log(`Servidor en funcionamiento en el puerto: ${port}`);
