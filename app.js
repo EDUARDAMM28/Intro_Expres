c2ce01884c8edbae2795e9b169dabd519dd8c232
pulidolaguna4-del <pulidolaguna4@gmail.com>
2026-08-28 09:51:31 -0500

    endpoins para verificar datos

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ import {configDotenv} from "dotenv"
 configDotenv()
 
 const app = express();
-const port = process.env.PUERTO || 2008
+const port = process.env.PUERTO || 3030
+//uso de middleware body-parse
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send('Aprendiendo express, ficha 3407181, ADSO en el SENA 31 julio');
@@ -19,7 +21,7 @@ res.send(`<h1>Otro ejemplo de ruta</h1
 })
 
 app.get("/ruta2", (req, res)=>{
-  res.json({"Nombre": "Vanessa", "Apellido": "Ospitia", "Cargo": "Aprendiz"})
+  res.json({"Nombre": "Eduard", "Apellido": "Martinez", "Cargo": "Aprendiz"})
 })
 
 app.get("/ruta3:aprendiz/:otrodato", (req, res)=>{
@@ -38,7 +40,50 @@ app.get("/ruta4", (req, res)=>{
   `)
 })
 
+//endpoins para enviar datos
+app.post("/ruta2", (req, res)=>{
+  const todosDatos = req.body
+  const name = req.body.Nombre
+  const lastname = req.body.Cargo
+  res.status(201).json({Datos: todosDatos, Nombre:name ,
+    Cargo: lastname})
+}
+)
 
+//endpoin para realizar 
+app.post("/login", (req, res) => {
+
+    const usuario = req.body.usuario
+    const perfil = req.body.perfil
+    const contraseña = req.body.contraseña
+
+    // Validar si faltan datos
+    if (!usuario, !perfil, !contraseña) {
+        return res.status(400).json({
+            mensaje: "Faltan datos. Debe enviar usuario, perfil y contraseña"
+        })
+    }
+
+    // Validar el perfil
+    if (perfil === "admin") {
+        return res.status(200).json({
+            mensaje: `Bienvenido ${usuario}, ha ingresado como administrador`
+        })
+    }
+
+    if (perfil === "user") {
+        return res.status(200).json({
+            mensaje: `Bienvenido ${usuario}, ha ingresado como usuario`
+        })
+    }
+
+    // Si el perfil no es válido
+    return res.status(403).json({
+        mensaje: "No tiene acceso. El perfil ingresado no es válido"
+    })
+})
 app.listen(port, function(){ 
     console.log( `SERVIDOR: http://localhost:${port}`); 
 });
+
+
