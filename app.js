8dbd84a1fc6d346863187f6b9ea550ad7fc88935
darwin <pulidolaguna4@gmail.com>
2026-09-11 08:44:35 -0500

    endpoin para crear aprendiz con from json utilizando  middleware bodyparse

--- app.js
+++ app.js
@@ -6,12 +6,19 @@ const port = process.env.port || 3000;
 const sistemaArchivo = require('fs')
 const ruta = require('path')
 const rutaArchivoJson = ruta.join(__dirname, "datos.json");
+//importar libreria para subir archivos
+const multer = require("multer")
+
+//millwear
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+
 //endpoin raiz
 app.get("/", function(req, res){
-  res.send('API - REST APRENDICES');
+  res.send('API - REST APRENDICES')
 })
 
-//endpoin para ver los datos del archivo
+
 app.get("/api/aprendices", (req, res)=> {
   //datos viene del archivo
   sistemaArchivo.readFile(rutaArchivoJson, 'utf-8', (error, datos)=>{
@@ -23,11 +30,27 @@ app.get("/api/aprendices", (req, res)=> {
   })
 }) 
 
-//endpoin para crear aprendices
-app.post("/api/aprendices",(res, req)=>{
-  res.json({Mensaje: "trabajando en el endpoin"})
-})
+//endpoin para ver los datos del archivo
+app.post("/api/aprendices", (req, res)=> {
+  const nuevoAprendiz = req.body
+  //datos viene del archivo
+  sistemaArchivo.readFile(rutaArchivoJson, 'utf-8', (error, datos)=>{
+    if(error){
+      return res.json({Error:"no se puede leer los datos"})
+    }
+    const listaAprendices = JSON.parse(datos)
+    //agregar el nuevo aprendiz
+    listaAprendices.push(nuevoAprendiz)
+    //escribir en el archivo
+    sistemaArchivo.writeFile(rutaArchivoJson, JSON.stringify(listaAprendices, null, 2), (error) => {
+      if (error) {
+        res.status(500).json({Error: "Nose puede registrar el aprendiz"})
+      }
+      res.status(201).json({Mensaje:"Aprendiz agregado correctamente"})
+    })
+  })
+}) 
 
-app.listen(port, () => {
+app.listen(port, function(){
   console.log(`Servidor en funcionamiento en el puerto: http://localhost:${port}`);
 });
